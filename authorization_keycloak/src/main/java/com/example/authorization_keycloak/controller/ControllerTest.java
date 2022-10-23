21977edce39558cdad2e41173cd0872b92b1803a
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-10-23 18:28:09 +0600

    created authorization implementation

--- authorization_keycloak/src/main/java/com/example/authorization_keycloak/controller/ControllerTest.java
+++ authorization_keycloak/src/main/java/com/example/authorization_keycloak/controller/ControllerTest.java
@@ -5,17 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping("/v1")
 public class ControllerTest {
 
     @GetMapping("/adminonly")
+    @RolesAllowed("admin")
     public ResponseEntity<String> getAdminResponse() {
         return ResponseEntity.ok("This is admin");
     }
 
     @GetMapping("/teacheronly")
+    @RolesAllowed("teacher")
     public ResponseEntity<String> getTeacherResponse() {
         return ResponseEntity.ok("this is teacher");
+    }
+
+    @GetMapping("/studentonly")
+    @RolesAllowed("student")
+    public ResponseEntity<String> getStudentResponse() {
+        return ResponseEntity.ok("this is student");
     }
 }
