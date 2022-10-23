f5121aba6e9485538e67629415eb1e7a06747f43
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-10-23 17:29:09 +0600

    created authorization project

---
+++ authorization_keycloak/src/main/java/com/example/authorization_keycloak/AuthorizationKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorization_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationKeycloakApplication.class, args);
+    }
+
+}
---
+++ authorization_keycloak/src/test/java/com/example/authorization_keycloak/AuthorizationKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorization_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
